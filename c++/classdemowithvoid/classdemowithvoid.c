286b302263326206e8517bf85a8eb1d65286b956
Bhavani R <bhavaniai01@brightskills.com>
2024-01-22 13:14:55 +0530

    c++ files

---
+++ c++/classdemowithvoid/classdemowithvoid.c
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class myclass
+
+{
+public:
+    int a,b;
+    int add()
+};
+
+    int myclass::add(int a,b){
+
+    }
+
+    int main()
+    {
+
+        myclass;
+        cin>>a>>b;
+        a=a+b;
+        cout<<a;
+        return 0;
+    }
+
+
+
