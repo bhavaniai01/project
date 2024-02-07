eea46d2c13ba4b2b1f915c02ce733a8d996c7f7f
Bhavani R <bhavaniai01@brightskills.com>
2024-02-07 10:28:29 +0530

    python files

---
+++ PYTHON/Pythondemo/first.py
@@ -0,0 +1,117 @@
+"""
+ print("hellow world");
+print("welcome my page");
+x=10;
+y=15;
+print(z);
+z=x-y;
+print(z);
+z=x*y;
+print(z);
+z=x/y;
+print(z);
+print(type(z));
+z=x%y;
+print(z);
+print(type(z));
+z=x+1;
+print(z);
+z=y-1;
+print(z);
+
+a=10;
+b=25;
+if b<a:
+print("a is less than b");
+else:
+	print("b is greater than a"); 
+	"""
+"""
+a=input("Enter value a: ");
+print(a);
+b=input("Enter value b");
+print(b);
+a=int(a);
+b=int(b);
+c=a+b;
+print ("added value a+b",c);
+"""
+
+# x=5;
+# if x<7:
+#     print ("x is less than")
+# elif x>6:
+#     print("elif is 6")
+# else:
+#     print(end)
+faviorte_season="summer"
+# if faviorte_season == "winter":
+#      print("This ismyb faviorte season")
+# elif faviorte_season == "summer":
+#      print("summer is amazing")
+# else:
+#      print("mom favorite season is summer")
+
+# for i in range(4):
+#     print(i)
+    
+# for j in range(15):
+#   print(j *2) 
+# for num in range(6):
+#  	print("\t ***" * num)
+
+# my_dict = {"a":1, "b": 2, "c":3}
+# for key ,value in my_dict.items():
+# 	print(key,value)
+
+# 	my_list= [1,2,3]
+
+# name="BHAVANI"
+# print(name.lower())
+
+# name="bhavani"
+# print(name.title())
+
+# name="bhava,bhavani"
+# print(name.title())
+
+# word = "Hello world"
+# for value , count in enumerate(word):
+#     print(value,count)
+	 
+	 
+# bike="dio"
+
+# if bike == "KTM":
+# 	print("Bike is KTM")
+# elif bike== "Yamaha":
+# 	print("Bike is Yamaha")
+# elif bike == "dio":
+# 	print("Bike is dio")
+# else:
+# 	print("choose the correect answer")	
+
+def python_switch(a):
+	match a: 
+		case 0:
+			print("value is Zero")
+		case 1:
+			print ("value is one")
+		case 2:
+			print("value is 2")
+		case 3:
+			print("value is 3")
+		case default:
+			print("not value")	
+      
+python_switch(2)
+
+
+
+
+
+
+
+
+
+
